aac43466505eaa126aa473e5ee2cc1aa9da4e612
hariv1 <hariv@rose-hulman.edu>
2018-01-08 12:14:50 -0500

    DONE

--- src/practice_problem1.py
+++ src/practice_problem1.py
@@ -45,7 +45,7 @@ def main():
     run_test_shrink()
     run_test_double_then_shrink()
     run_test_reset()
-#     run_test_steal()
+    run_test_steal()
 #     run_test_get_history()
 #     run_test_combined_box()
 
@@ -404,7 +404,7 @@ class Box(object):
           :type other_box: Box
         """
         # --------------------------------------------------------------
-        # TODO: 8. Implement and test this function.
+        # DONE: 8. Implement and test this function.
         #     The testing code is already written for you (above).
         # --------------------------------------------------------------
         # --------------------------------------------------------------
@@ -416,6 +416,9 @@ class Box(object):
         # FOR FULL CREDIT, YOUR SOLUTION MUST BE NO MORE THAN
         #    ** TWO **   LINES OF CODE.
         ################################################################
+
+        sub_string = self.append_string(other_box.contents)
+        other_box.contents = sub_string
 
     def get_history(self):
         """
